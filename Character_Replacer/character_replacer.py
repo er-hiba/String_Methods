edc14a51006aadad6bf394decd4a424e5b2d6d2a
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-10-18 23:02:34 +0100

    :)

---
+++ Character_Replacer/character_replacer.py
@@ -0,0 +1,9 @@
+string = input("Enter a string: ")
+
+c = input("Enter the character you want to replace: ")
+
+C = input("Enter the character you want to replace it with: ")
+
+x = string.replace(c, C)
+
+print("The updated string is:",x)
